327e1d1245a352d357abd6cbfa85934db5a21eba
codriv <combodriver4@yahoo.at>
2022-07-17 08:39:00 +0200

    init

---
+++ src/main/java/org/training360/sv2022jvjbfzarovizsga/Sv2AdvFinalExamApplication.java
@@ -0,0 +1,13 @@
+package org.training360.sv2022jvjbfzarovizsga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv2AdvFinalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sv2AdvFinalExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/training360/sv2022jvjbfzarovizsga/Sv2AdvFinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training360.sv2022jvjbfzarovizsga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sv2AdvFinalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
